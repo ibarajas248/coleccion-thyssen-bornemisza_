a1ec30f61a906f6f72e7b058ad5e0d71b28213e8
ibarajas248 <ibarajas@unal.edu.co>
2024-11-19 13:19:45 -0500

    archivo ok_revision pendiente

--- scraping.py
+++ scraping.py
@@ -1,41 +1,163 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from lxml import etree
+
+
+# Función para raspar los detalles adicionales de una obra
+def scrape_additional_info(link):
+    response = requests.get(link)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        try:
+            # Convertir el árbol BeautifulSoup a un árbol lxml para usar XPath
+            dom = etree.HTML(str(soup))
+
+            # Buscar el div padre con id 'rs_artwork_description'
+            parent_div = soup.find("div", id="rs_artwork_description")
+            additional_text = "Información adicional no disponible"
+
+            if parent_div:
+                # Buscar dentro del padre el div hijo con clase 'u-mb@xs'
+                child_div = parent_div.find("div", class_="u-mb@xs")
+                if child_div:
+                    additional_text = child_div.get_text(separator="\n").strip()
+
+            # Localizar el tamaño usando XPath
+            size_xpath = dom.xpath('//*[@id="main_content"]/div[2]/article/div[1]/div[2]/div/div/div[2]/div[2]/div[1]/div')
+            size_text = "Tamaño no disponible"
+            if size_xpath:
+                # Obtener el texto del primer nodo encontrado
+                size_text = size_xpath[0].text.strip()
+
+            # Localizar la técnica usando XPath
+            tecnica_xpath = dom.xpath('//*[@id="main_content"]/div[2]/article/div[1]/div[2]/div/div/div[2]/div[2]/div[1]/text()')
+            tecnica_text = "Técnica no disponible"
+            if tecnica_xpath:
+                # Obtener el texto del primer nodo encontrado
+                tecnica_text = tecnica_xpath[0].strip()
+
+            # Localizar la ubicación usando XPath
+            ubicacion_xpath = dom.xpath('//*[@id="main_content"]/div[2]/article/div[1]/div[2]/div/div/div[2]/div[2]/div[2]/div')
+            ubicacion_text = "Ubicación no disponible"
+            if ubicacion_xpath:
+                # Obtener el texto del primer nodo encontrado
+                ubicacion_text = ubicacion_xpath[0].text.strip()
+                #print(ubicacion_text)
+            # Buscar el div que contiene el enlace de la imagen en el atributo 'data-href'
+            image_div = soup.find("div",
+                                  class_="u-absolute u-inset-0 u-z-0 u-bg-gray-lightest u-static@print is-hidden js-zoom-map-viewer")
+            image_url = "Imagen no disponible"
+            if image_div:
+                # Extraer el enlace de la imagen desde el atributo 'data-href'
+                image_url = image_div.get("data-href", "Imagen no disponible")
+                #print(image_url)  # Para verificar si se está obteniendo correctamente la URL
+
+            # Localizar el texto adicional utilizando XPath
+            codigo_inventario = dom.xpath(
+                '//*[@id="main_content"]/div[2]/article/div[1]/div[2]/div/div/div[2]/div[2]/div[3]')
+            codigo_inventario_text = "Texto adicional no disponible"
+            if codigo_inventario:
+                # Concatenar todo el texto dentro del nodo encontrado
+                codigo_inventario_text = " ".join([node.strip() for node in codigo_inventario[0].itertext()])
+
+            # Localizar la sala usando XPath
+            sala_xpath = dom.xpath(
+                '//*[@id="main_content"]/div[2]/article/div[1]/div[2]/div/div/div[2]/div[2]/div[4]/a')
+            sala_text = "Sala no disponible"
+            if sala_xpath:
+                # Obtener el texto o el enlace del nodo encontrado
+                sala_text = sala_xpath[0].text.strip() if sala_xpath[0].text.strip() else sala_xpath[0].get("href",
+                                                                                                            "Sala no disponible")
+
+
+            return {
+                "additional_info": additional_text,
+                "size_info": size_text,
+                "technique_info": tecnica_text,
+                "ubicacion_info": ubicacion_text,  # Añadir ubicación aquí
+                "image_url": image_url,
+                "codigo_inventario_text":codigo_inventario_text,
+                "sala_text":sala_text
+
+            }
+
+        except Exception as e:
+            print(f'Error al obtener información adicional: {e}')
+            return {
+                "additional_info": "Error al obtener información adicional",
+                "size_info": "Error",
+                "technique_info": "Error",
+                "ubicacion_info": "Error"  # Si ocurre un error, devolver "Error"
+            }
+    else:
+        print(f'Error al solicitar el enlace {link}: {response.status_code}')
+        return {
+            "additional_info": "Error al solicitar información adicional",
+            "size_info": "Error",
+            "technique_info": "Error",
+            "ubicacion_info": "Error"  # Si no se puede obtener la información, devolver "Error"
+        }
+
+
+
 
 # Función para raspar los datos de una sola página
 def scrape_page(page_number):
     url = f'https://www.museothyssen.org/buscador/tipo/obra?page={page_number}'
-
-    # Realizar la solicitud HTTP
     response = requests.get(url)
-
-    # Verificar que la solicitud fue exitosa
     if response.status_code == 200:
-        # Crear objeto BeautifulSoup
         soup = BeautifulSoup(response.content, 'html.parser')
-
-        # Encontrar todos los div con la clase 'snippet__body'
         snippets = soup.find_all('div', class_='snippet__body')
-
-        # Lista para almacenar los datos
         data = []
-
-        # Extraer información de cada snippet
         for snippet in snippets:
             try:
-                # Encontrar el enlace
+                # Obtener el enlace
                 link = snippet.find('a', class_='snippet__caption js-snippet-link')['href']
                 title = snippet.find('div', class_='snippet__title').get_text(strip=True)
                 subtitle = snippet.find('div', class_='snippet__subtitle').get_text(strip=True)
                 text = snippet.find('div', class_='snippet__text').get_text(strip=True)
+                full_link = link
+
+                # Hacer el segundo scraping para obtener la información adicional
+                additional_data = scrape_additional_info(full_link)
+                additional_info = additional_data["additional_info"]
+                size_info = additional_data["size_info"]
+                tecnica = additional_data["technique_info"]
+                ubicacion = additional_data["ubicacion_info"]
+                image_url = additional_data["image_url"]
+                codigo_inventario= additional_data["codigo_inventario_text"]
+                sala= additional_data["sala_text"]
+
 
                 # Añadir los datos a la lista
                 data.append({
                     'Autor': title,
                     'Título': subtitle,
                     'Fecha': text,
-                    'Enlace': f'https://www.museothyssen.org{link}'
+                    'Enlace': full_link,
+                    'Información adicional': additional_info,
+                    'Tamaño': size_info,
+                    'tecnica': tecnica,
+                    'ubicacion': ubicacion,
+                    'image_url':image_url,
+                    'codigo_inventario':codigo_inventario,
+                    'sala':sala
+
                 })
+
+                print("----------------------------")
+                print(title)
+                print(subtitle)
+                print(text)
+                print(f'Información adicional: {additional_info}')
+                print(f'Tamaño: {size_info}')
+                print(f'tecnica: {tecnica}')
+                print(f'ubicacion: {ubicacion}')
+                print(f'image_url: {image_url}')
+                print(f'codigo_inventario: {codigo_inventario}')
+                print(f'sala: {sala}')
+                print("----------------------------")
             except Exception as e:
                 print(f'Error al procesar un snippet: {e}')
         return data
@@ -43,8 +165,9 @@ def scrape_page(page_number):
         print(f'Error al realizar la solicitud a la página {page_number}: {response.status_code}')
         return []
 
+
 # Número total de páginas a raspar
-total_pages = 5  # Cambia este valor según el número de páginas que deseas raspar
+total_pages = 1  # Cambia este valor según el número de páginas que deseas raspar
 
 # Lista para almacenar todos los datos
 all_data = []
@@ -60,6 +183,6 @@ for page in range(1, total_pages + 1):
 df = pd.DataFrame(all_data)
 
 # Guardar el DataFrame en un archivo Excel
-df.to_excel('museo_obras.xlsx', index=False)
+df.to_excel('museo_obras_con_info_adicional.xlsx', index=False)
 
 print('Archivo Excel creado con éxito.')
